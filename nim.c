db80be72f27cb870573dd3aaa1a21fa99649f9fb
Rashi Mathur <rashi@uoguelph.ca>
2021-11-18 10:30:00 -0500

    added the nim.c file

---
+++ nim.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#include "nim.h"
+
+/*************************************main****************************************/
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
+
+
+
+/*************************************function1*************************************/
+
+// int *new_board( int board_size ){
+	
+// }
+
+/*************************************function2*************************************/
+// struct node *mk_nim_hash( int max_hash, int board_size, int *start_board ){
+
+// }
+
+/*************************************function3*************************************/
+// void free_board( int *board ){
+
+// }
+/*************************************function4*************************************/
+// void free_nim_hash( int max_hash, struct node *nim_hash ){
+
+// }
+/*************************************function5*************************************/
+// int *board_from_argv( int board_size, char **argv ){
+
+// }
+/*************************************function6*************************************/
+// int *copy_board( int board_size, int *board ){
+
+// }
+/*************************************function7*************************************/
+// int game_over( int board_size, int *board ){
+
+// }
+/*************************************function8*************************************/
+// void join_graph( struct node *nim_hash, int hash, int board_size, 
+//     int *start_board ){
+
+// 	}
+/*************************************function9*************************************/
+// int compute_numsum( int board_size, int *board ){
+
+// }
+/*************************************function10*************************************/
+// void print_board( int board_size, int *board ){
+
+// }
+/*************************************function11*************************************/
+// int compute_max_hash( int board_size, int *start_board ){
+
+// }
+/*************************************function12*************************************/
+// int board2hash( int board_size, int *start_board, int *current_board ){
+
+// }
+/*************************************function13*************************************/
+// int *hash2board( int board_size, int *start_board, int hash ){
+	
+// }
+/*************************************DONE*************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
